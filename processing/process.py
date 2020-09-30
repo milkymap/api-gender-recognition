a2b7ce43a2b0b57cc76356547db6b0ba97bfeaf9
Ibrahima BA <prestataire.0140781@ouest-france.fr>
2020-09-30 16:39:44 +0200

    [Update] processing and recognizer | initialize source code

---
+++ processing/process.py
@@ -0,0 +1,14 @@
+import itertools as it 
+import functools as ft 
+
+import numpy as np 
+import operator as op 
+
+import cv2 
+
+import os 
+import os.path as path 
+import glob 
+
+import json 
+
---
+++ recognizer/recognize.py
@@ -0,0 +1,6 @@
+import cv2 
+import numpy as np 
+
+class Recognizer:
+    def __init__(self):
+        pass 
